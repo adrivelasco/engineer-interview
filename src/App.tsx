fde55b471781cb399759656bd3f7ecf44523a192
Adrian <lonadr@amazon.com>
2021-09-25 19:57:05 -0300

    refactor: Adjust Layout in order to use Chakra like other components do

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import React from 'react'
+import { Box, Button, ChakraProvider, Flex, Text } from '@chakra-ui/react'
+
 import { ChallengeComponent } from './ChallengeComponent'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ marginLeft: 25, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-          <h1>Welcome To The Every.io Code Challenge.</h1>
-          <div style={{ marginLeft: 50 }}>
-            <a
-              target="_blank"
-              href="https://www.figma.com/proto/kd49ArXbBt0vi1kBSLkmC1/Code-Challenge?node-id=1%3A2&scaling=min-zoom&page-id=0%3A1"
-              style={{ color: 'white', background: 'black', padding: 8, borderRadius: 4, display: 'inline-block' }}
-              rel="noreferrer">
-              Checkout the Prototype
-            </a>
-          </div>
-        </div>
-      </header>
-      <main
-        style={{
-          width: 'calc(100vw - 50px)',
-          margin: '0 auto',
-          maxHeight: '100%',
-          background: '#fcfcfc',
-          border: '1px solid #000000',
-          boxSizing: 'border-box',
-          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-          borderRadius: 40,
-        }}>
+export const App = () => (
+  <ChakraProvider>
+    <Box width="100%" maxWidth={1400} mx="auto" py={6}>
+      <Box as="header" py={5} mb={3}>
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Text fontWeight="bold" fontSize="x-large">
+            Welcome To The Every.io Code Challenge.
+          </Text>
+          <Button
+            as="a"
+            bg="black"
+            colorScheme="black"
+            href="https://www.figma.com/proto/kd49ArXbBt0vi1kBSLkmC1/Code-Challenge?node-id=1%3A2&scaling=min-zoom&page-id=0%3A1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Checkout the Prototype
+          </Button>
+        </Flex>
+      </Box>
+      <Box
+        as="main"
+        border="1px solid"
+        borderColor="blackAlpha.300"
+        rounded="md"
+        shadow="md"
+      >
         <ChallengeComponent />
-      </main>
-    </div>
-  )
-}
-
-export default App
+      </Box>
+    </Box>
+  </ChakraProvider>
+);
