a8b7c37e4485d800941d925d5bc05ae7c5a989bf
Adrian <lonadr@amazon.com>
2021-09-27 01:11:42 -0300

    add: Use TaskBoardProvider on ChallengeComponent

--- src/components/ChallengeComponent/ChallengeComponent.tsx
+++ src/components/ChallengeComponent/ChallengeComponent.tsx
@@ -2,27 +2,63 @@ import { Box } from "@chakra-ui/react";
 
 import { Board } from "../Board";
 import { Column } from "../Column";
-import { FormCreateTask } from "../FormCreateTask";
+import { FormCreateTask, FormFields } from "../FormCreateTask";
 import { Task } from "../Task";
+import { TaskBoardProvider, useTaskBoardContext } from "../TaskBoardProvider";
 
-export const ChallengeComponent = () => {
+const ChallengeComponent = () => {
+  const { tasks, status, addTask, moveToRight, moveToLeft } =
+    useTaskBoardContext();
+
+  const handleAddTask = ({ description }: FormFields) => {
+    addTask({ description });
+  };
+
   return (
     <Box px={10} py={10} height="auto">
       <Board>
-        <Column title="To Do">
-          <Task description="Move the land" />
-        </Column>
-        <Column title="In Progress">
-          <Task description="Move the land" />
-        </Column>
-        <Column title="Done">
-          <Task description="Move the land" />
-        </Column>
-      </Board>
+        {status.map(({ id, name }, index) => {
+          const tasksByStatus = tasks.filter(({ status }) => id === status);
 
+          const isFirstColumn = index === 0;
+          const isLastColumn = index === status.length - 1;
+
+          return (
+            <Column key={id} title={name}>
+              {tasksByStatus.map(({ id, description }) => {
+                const handleOnMoveLeft = () => {
+                  moveToLeft(id);
+                };
+
+                const handleOnMoveRight = () => {
+                  moveToRight(id);
+                };
+
+                return (
+                  <Task
+                    description={description}
+                    key={id}
+                    mb={2}
+                    onMoveLeft={!isFirstColumn ? handleOnMoveLeft : undefined}
+                    onMoveRight={!isLastColumn ? handleOnMoveRight : undefined}
+                  />
+                );
+              })}
+            </Column>
+          );
+        })}
+      </Board>
       <Box position="relative" maxWidth={400}>
-        <FormCreateTask />
+        <FormCreateTask onSubmit={handleAddTask} />
       </Box>
     </Box>
   );
 };
+
+const ChallengeComponentWithContext = () => (
+  <TaskBoardProvider>
+    <ChallengeComponent />
+  </TaskBoardProvider>
+);
+
+export { ChallengeComponentWithContext as ChallengeComponent };
