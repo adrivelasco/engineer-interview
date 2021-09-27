d4cde15a9e466b006bab5dd1130a5c6c9ab6137b
Adrian <lonadr@amazon.com>
2021-09-27 01:10:48 -0300

    add: Use react-hook-form for validating form

--- src/components/FormCreateTask/FormCreateTask.tsx
+++ src/components/FormCreateTask/FormCreateTask.tsx
@@ -1,15 +1,54 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Button, Flex, FlexProps, Input } from "@chakra-ui/react";
+import { Box, BoxProps, Button, Flex, Input, Text } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
-export interface FormCreateTaskProps extends FlexProps {}
+export interface FormFields {
+  description: string;
+}
 
-export const FormCreateTask = ({ ...props }: FormCreateTaskProps) => {
+export interface FormCreateTaskProps extends Omit<BoxProps, "onSubmit"> {
+  onSubmit?: (values: FormFields) => void;
+}
+
+export const FormCreateTask = ({
+  onSubmit: onSubmitProp,
+  ...props
+}: FormCreateTaskProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      description: "",
+    },
+  });
+
+  const onSubmit = (values: FormFields) => {
+    reset();
+    onSubmitProp?.(values);
+  };
+
   return (
-    <Flex alignItems="center" py={2}>
-      <Input flex={1} mr={2} placeholder="Add Task" />
-      <Button colorScheme="blue" type="submit">
-        <AddIcon />
-      </Button>
-    </Flex>
+    <Box as="form" onSubmit={handleSubmit(onSubmit)}>
+      <Flex alignItems="center" py={2} {...props}>
+        <Input
+          flex={1}
+          isInvalid={Boolean(errors.description)}
+          mr={2}
+          placeholder="Add Task"
+          {...register("description", { required: true, minLength: 1 })}
+        />
+        <Button colorScheme="blue" type="submit">
+          <AddIcon />
+        </Button>
+      </Flex>
+      {errors.description && (
+        <Text color="red.500" fontSize="sm" fontWeight="medium">
+          This field is required
+        </Text>
+      )}
+    </Box>
   );
 };
