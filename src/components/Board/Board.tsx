a27308b07e1c925ccf35d5e8b1eeb7ce302fc4ee
Adrian <lonadr@amazon.com>
2021-09-27 22:52:18 -0300

    refactor: Move file of default values and sort props on Board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,10 +2,10 @@ import { Flex, FlexProps } from "@chakra-ui/react";
 
 export const Board = ({ ...props }: FlexProps) => (
   <Flex
-    py={2}
-    height={{ base: "1200px", md: "500px" }}
-    w="full"
     flexDirection={{ base: "column", md: "row" }}
+    height={{ base: "1200px", md: "500px" }}
+    py={2}
+    w="full"
     {...props}
   />
 );
